42c25206160653c105c36f1a9d05345b25ce0178
Joseph Shapiro <joeyashapiro@gmail.com>
2026-02-08 15:56:22 -0600

    build my game

---
+++ build.zig
@@ -0,0 +1,49 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    // Get raylib dependency and compile it
+    const raylib_dep = b.dependency("raylib", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // Get the compiled raylib library
+    const raylib = raylib_dep.artifact("raylib");
+
+    // Create executable from main.c
+    const exe = b.addExecutable(.{
+        .name = "calamari",
+        .root_module = b.createModule(.{
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+
+    // Add main.c source
+    exe.addCSourceFile(.{
+        .file = b.path("main.c"),
+        .flags = &.{"-std=c99"},
+    });
+
+    // Link with raylib
+    exe.linkLibrary(raylib);
+
+    // Link C library
+    exe.linkLibC();
+
+    // Install executable
+    b.installArtifact(exe);
+
+    // Create a run step
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the game");
+    run_step.dependOn(&run_cmd.step);
+}
